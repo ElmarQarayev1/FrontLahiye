fd2029849bd0f4848f8ad70e03e8f7c707cc54bd
elmar <elmar@elmar-Air.bbrouter>
2024-04-10 21:36:23 +0400

    courses geldi esas sehifeye

---
+++ Front/js/index.js
@@ -0,0 +1,28 @@
+loadCourses();
+function createCourse(course){
+    let courseBox=`<div class="col-lg-4 col-md-6">
+    <div class="single-course pb-5 mb-70">
+        <div class="course-img">
+            <a href="course-details.html"><img src="${course.imgPath}" alt="course">
+            </a>
+        </div>
+        <div class="coursecontent pl-4 pt-4">
+            <h3><a href="course-details.html">${course.name}</a></h3>
+            <p>${course.description}</p>
+            <button class="btn btn-lg"><a data-id="${course.id}" href="course-details.html">read more</a></button>
+        </div>   
+    </div>
+</div> `
+ return courseBox;
+}
+function loadCourses(){
+    let coursesBox=document.getElementById("courseId");
+    fetch("http://localhost:5000/courses")
+    .then(response=>response.json())
+    .then(data=>{
+        data.forEach(course => {
+            let createdCourseElem=createCourse(course);
+            coursesBox.innerHTML+=createdCourseElem;    
+        });
+    })
+}
