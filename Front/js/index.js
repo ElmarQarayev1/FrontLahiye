b76cb88978b3799f9cae883b63c7c3a0a033f1b2
elmar <elmar@elmar-Air.bbrouter>
2024-04-11 18:33:00 +0400

    read more hissesi yazildi

--- Front/js/index.js
+++ Front/js/index.js
@@ -1,34 +1,45 @@
-loadCourses();
-filterCourses();
-function createCourse(course){
-    let courseBox=`<div class="col-lg-4 col-md-6">
-    <div class="single-course pb-5 mb-70">
-        <div class="course-img">
-            <a href="course-details.html"><img src="${course.imgPath}" alt="course">
-            </a>
-        </div>
-        <div class="coursecontent pl-4 pt-4">
-            <h3><a href="course-details.html">${course.name}</a></h3>
-            <p>${course.description}</p>
-            <button class="btn btn-lg"><a data-id="${course.id}" href="course-details.html?id=${course.id}">read more</a></button>
-        </div>   
-    </div>
-</div> `
- return courseBox;
-}
-let coursesBox=document.getElementById("courseId");
+let courses = [];
+let visibleCourses = 6;
+let coursesBox = document.getElementById("courseId");
 
-function loadCourses(){
-    
-    fetch("http://localhost:5000/courses")
-    .then(response=>response.json())
-    .then(data=>{
-        data.forEach(course => {
-            let createdCourseElem=createCourse(course);
-            coursesBox.innerHTML+=createdCourseElem;    
-        });
-    })
+function createCourse(course) {
+    let courseBox = `<div class="col-lg-4 col-md-6">
+        <div class="single-course pb-5 mb-70">
+            <div class="course-img">
+                <a href="course-details.html?id=${course.id}"><img src="${course.imgPath}" alt="course"></a>
+            </div>
+            <div class="coursecontent pl-4 pt-4">
+                <h3><a href="course-details.html?id=${course.id}">${course.name}</a></h3>
+                <p>${course.description}</p>
+                <button class="btn btn-lg"><a data-id="${course.id}" href="course-details.html?id=${course.id}">read more</a></button>
+            </div>
+        </div>
+    </div>`;
+
+    return courseBox;
 }
+
+function loadMoreCourses() {
+    fetch("http://localhost:5000/courses")
+    .then(response => response.json())
+    .then(data => {
+        courses = data;
+        showCourses();
+    });
+}
+function showCourses() {
+    coursesBox.innerHTML = "";
+    for (let i = 0; i < visibleCourses && i < courses.length; i++) {
+        let createdCourseElem = createCourse(courses[i]);
+        coursesBox.innerHTML += createdCourseElem;
+    }
+    if (visibleCourses >= courses.length) {
+        document.getElementById("loadMoreBtn").style.display = "none";
+    }
+}
+
+document.getElementById("loadMoreBtn").addEventListener("click", loadMoreCourses);
+
 function filterCourses() {
     document.getElementById("searchbtn").addEventListener("click", function(e) {
         let inputValue = document.getElementById("searchCourse").value.toLowerCase().trim();
@@ -40,12 +51,9 @@ function filterCourses() {
                 return response.json();
             })
             .then(filteredCourses => {
-                coursesBox.innerHTML = ""; 
-                filteredCourses.forEach(course => {
-                    let createdCourseElem = createCourse(course);
-                    coursesBox.innerHTML+=createdCourseElem;
-                    document.getElementById("searchCourse").value = ""; 
-                });
+                courses = filteredCourses;
+                showCourses();
+                document.getElementById("searchCourse").value = ""; 
             })
             .catch(error => {
                 console.error("Error:", error);
@@ -72,20 +80,32 @@ document.addEventListener("DOMContentLoaded", function() {
     scrollToTopBtn.style.right = "20px";
     scrollToTopBtn.style.display = "none";
 });
+
 window.addEventListener("scroll", function() {
     let navbar = document.querySelector(".navbar2");
-    let imgCourse=this.document.querySelector(".imgCourse");
+    let imgCourse = this.document.querySelector(".imgCourse");
 
     if (window.scrollY > 5) { 
-        imgCourse.style.paddingTop="0px";
+        imgCourse.style.paddingTop = "0px";
         navbar.classList.add("scrolled");
         navbar.classList.add("gotoTop");
-        imgCourse.classList.add("gotoTop")
+        imgCourse.classList.add("gotoTop");
     } else {
-        imgCourse.style.paddingTop="140px";
+        imgCourse.style.paddingTop = "140px";
         navbar.classList.remove("scrolled");
         navbar.classList.remove("gotoTop");
     }
 });
 
+function loadMore() {
+    visibleCourses += 3; 
+    showCourses(); 
+    if (visibleCourses >= courses.length) {
+        document.getElementById("loadMoreBtn").style.display = "none";
+    }
+}
 
+document.getElementById("loadMoreBtn").addEventListener("click", loadMore); 
+
+loadMoreCourses(); 
+filterCourses(); 
